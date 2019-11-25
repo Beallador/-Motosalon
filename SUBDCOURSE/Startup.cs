dad3b93c367b24ec9797ff342d9ee17985fc6bec
MaximLopatsin <51212123+MaximLopatsin@users.noreply.github.com>
2019-11-25 19:24:44 +0300

    Revert "Dev"

--- SUBDCOURSE/Startup.cs
+++ SUBDCOURSE/Startup.cs
@@ -5,15 +5,13 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using SUBDCOURSE.Data;
 using SUBDCOURSE.Data.Interfaces;
-using SUBDCOURSE.Data.Models;
-using SUBDCOURSE.Data.Repository;
+using SUBDCOURSE.Data.Mocks;
 
 namespace SUBDCOURSE
 {
@@ -30,38 +28,22 @@ namespace SUBDCOURSE
         public IConfiguration Configuration { get; }
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
             string connection = Configuration.GetConnectionString("DefaultConnection");
 
             services.AddDbContext<AppDbContext>(options =>
                 options.UseSqlServer(connection));
-            services.AddTransient<IAllMoto, MotoRepository>();
-            services.AddTransient<IMotosCategory, CategoryRepository>();
-            services.AddTransient<IAllOrder, OrderRepository>();
-           
-            services.AddScoped(sp => ShopCart.GetCart(sp));
-
+            services.AddTransient<IAllMoto, MockMoto>();
+            services.AddTransient<IMotosCategory, MockCategory>();
             services.AddMvc(option => option.EnableEndpointRouting = false);
-            services.AddMemoryCache();
-            services.AddSession();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            app.UseSession();
-            //app.UseMvcWithDefaultRoute();
+            app.UseMvcWithDefaultRoute();
+            app.UseDeveloperExceptionPage();
             app.UseStaticFiles();
             app.UseStatusCodePages();
-            app.UseMvc(routes =>
-            {
-                routes.MapRoute(name: "default", template: "{controller=Home}/{action=Index}/{Id?}");
-                routes.MapRoute(name:"categoryFilter", template:"Moto/{action}/{category?}",defaults: new { Controller="Moto",action="Moto"});
-            });
         }
     }
 }
